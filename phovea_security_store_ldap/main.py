e194c66067dc33d981b74807448496f8632566f5
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-11-23 23:36:33 +0100

    improve output

--- phovea_security_store_ldap/main.py
+++ phovea_security_store_ldap/main.py
@@ -1,14 +1,18 @@
 def create(_):
   def _loop():
-    from .ldap import LDAPStore
+    from .ldap import LDAPStore, log
+    import logging
+    import getpass
+
+    log.setLevel(logging.DEBUG)
     store = LDAPStore()
     username = raw_input('username: ')
 
     while username:
-      password = raw_input('password: ')
+      password = getpass.getpass('password: ')
       user = store.login(username, dict(password=password))
       if user:
-        print('OK {u.id} name:{u.name} roles:{u.roles} dn:{u.dn}'.format(u=user))
+        print('OK {u.id} name:{u.name} roles:{u.roles} dn:{u.dn} {u.info} {u.groups}'.format(u=user))
       else:
         print('ERROR')
       username = raw_input('another username: ')
